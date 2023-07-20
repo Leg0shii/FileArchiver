efa0d1e6849fb8adc4da08b1c8cb0b6fc31508c1
0hnezahn <benjamin.mueller2@uni-rostock.de>
2023-07-20 10:10:58 +0200

    fix type

--- src/main/java/de/legoshi/main/FileData.java
+++ src/main/java/de/legoshi/main/FileData.java
@@ -3,29 +3,30 @@ package de.legoshi.main;
 import de.legoshi.util.TableObjectLC;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileData {
 
-    private ArrayList<TableObjectLC> arrayList;
-    private ArrayList<TableObjectLC> arrayListKM;
+    private List<TableObjectLC> arrayList;
+    private List<TableObjectLC> arrayListKM;
 
     public FileData() {
         this.arrayList = new ArrayList<>();
     }
 
-    public ArrayList<TableObjectLC> getArrayList() {
+    public List<TableObjectLC> getArrayList() {
         return arrayList;
     }
 
-    public ArrayList<TableObjectLC> getArrayListKM() {
+    public List<TableObjectLC> getArrayListKM() {
         return arrayListKM;
     }
 
-    public void setIdentityHashMap(ArrayList<TableObjectLC> arrayList) {
+    public void setIdentityHashMap(List<TableObjectLC> arrayList) {
         this.arrayList = arrayList;
     }
 
-    public void setIdentityHashMapKM(ArrayList<TableObjectLC> arrayListKM) {
+    public void setIdentityHashMapKM(List<TableObjectLC> arrayListKM) {
         this.arrayListKM = arrayListKM;
     }
 }
